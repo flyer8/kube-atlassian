4e4f46a20258cfc54c774385c99be6820a9d6af9
Sergey Shimanskiy <flyer8@yandex.ru>
2017-06-26 01:39:10 +0300

    Delete restart-nginx.sh

--- restart-nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# restart nginx
-kubectl delete configmap nginxconfigmap
-kubectl create configmap nginxconfigmap --from-file=/opt/atlassian/nginx/default.conf
-kubectl delete deploy nginx
-kubectl apply -f nginx/nginx.yaml
