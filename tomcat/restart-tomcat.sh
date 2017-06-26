923a3d962e3ce8f4bfbf6ac3472d584bb6eeb15a
Sergey Shimanskiy <flyer8@yandex.ru>
2017-06-26 13:33:40 +0000

    Tomcat mofofied

--- tomcat/restart-tomcat.sh
+++ tomcat/restart-tomcat.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # restart tomcat
 kubectl delete configmap tomcatconfigmap
-kubectl create configmap tomcatconfigmap --from-file=/opt/tomcat/conf
+kubectl create configmap tomcatconfigmap --from-file=/opt/atlassian/tomcat/conf
 kubectl delete configmap managerconfigmap
-kubectl create configmap managerconfigmap --from-file=/opt/tomcat/manager
+kubectl create configmap managerconfigmap --from-file=/opt/atlassian/tomcat/manager
 kubectl delete deploy tomcat
 kubectl apply -f tomcat.yaml
