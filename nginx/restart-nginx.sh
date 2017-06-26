9b1d6f8c4f7853a8e0fabff7ffc723cb5a415f2e
Sergey Shimanskiy <flyer8@yandex.ru>
2017-06-26 13:23:23 +0000

    Tomcat modified

--- nginx/restart-nginx.sh
+++ nginx/restart-nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # restart nginx
 kubectl delete configmap nginxconfigmap
-kubectl create configmap nginxconfigmap --from-file=/opt/atlassian/nginx/default.conf
+kubectl create configmap nginxconfigmap --from-file=/opt/nginx/default.conf
 kubectl delete deploy nginx
 kubectl apply -f nginx.yaml
--- tomcat/restart-tomcat.sh
+++ tomcat/restart-tomcat.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# restart nginx
+# restart tomcat
 kubectl delete configmap tomcatconfigmap
-kubectl create configmap tomcatconfigmap --from-file=/opt/atlassian/tomcat/conf
+kubectl create configmap tomcatconfigmap --from-file=/opt/tomcat/conf
+kubectl delete configmap managerconfigmap
+kubectl create configmap managerconfigmap --from-file=/opt/tomcat/manager
 kubectl delete deploy tomcat
 kubectl apply -f tomcat.yaml
