713f371d1e46c2bed0013e162827860edcf6d515
Sergey Shimanskiy <flyer8@yandex.ru>
2017-07-08 01:35:59 +0300

    Update restart-tomcat.sh

--- tomcat/restart-tomcat.sh
+++ tomcat/restart-tomcat.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # restart tomcat
 kubectl delete configmap tomcatconfigmap
-kubectl create configmap tomcatconfigmap --from-file=/opt/atlassian/tomcat/conf
+kubectl create configmap tomcatconfigmap --from-file=conf/
 kubectl delete configmap managerconfigmap
-kubectl create configmap managerconfigmap --from-file=/opt/atlassian/tomcat/manager
+kubectl create configmap managerconfigmap --from-file=manager/
 kubectl delete deploy tomcat
 kubectl apply -f tomcat.yaml
