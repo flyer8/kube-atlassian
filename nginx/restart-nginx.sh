1e7693d0096ccf6135ac4663bd72ff05ca879310
Sergey Shimanskiy <flyer8@yandex.ru>
2017-07-14 20:44:42 +0300

    nginx fixed

--- nginx/restart-nginx.sh
+++ nginx/restart-nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # restart nginx
-kubectl delete configmap nginxconfigmap
-kubectl create configmap nginxconfigmap --from-file=default.conf
+kubectl delete configmap nginx-cm
+kubectl create configmap nginx-cm --from-file=default.conf
 kubectl delete deploy nginx
-kubectl apply -f nginx.yaml
+kubectl apply -f nginx-base.yaml
