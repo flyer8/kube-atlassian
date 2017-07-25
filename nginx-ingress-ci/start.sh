143a67e4d0a6ad05cce96dbbb634c0fe84922df7
Sergey Shimanskiy <flyer8@yandex.ru>
2017-07-25 16:29:14 +0300

    Ingress moved

--- nginx-ingress-ci/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl create -f nginx-ingress-rc.yaml && \
-kubectl create -f jenkins.yaml && \
-kubectl create -f tomcat.yaml && \
-kubectl create -f ci-secret.yaml && \
-kubectl create -f ci-ingress.yaml
--- nginx-ingress-ci/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl delete rc nginx-ingress-rc && \
-kubectl delete rc,svc jenkins && \
-kubectl delete rc,svc tomcat && \
-kubectl delete secrets ci-secret && \
-kubectl delete ing ci-ingress
